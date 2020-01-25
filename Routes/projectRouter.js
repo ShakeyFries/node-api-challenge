ada510a70036e9afa95411334dc96d0d7e03ded4
Chris Daley <Daleys@Chriss-iMac.local>
2020-01-24 20:20:31 -0700

    finished projects routers with full CRUD

--- Routes/projectRouter.js
+++ Routes/projectRouter.js
@@ -7,7 +7,7 @@ const Action = require('.././data/helpers/actionModel');
 
 //========================== Get ============================================
 router.get('/', ( req,res ) => {
-  project.get(req.query)
+  project.get()
     .then(go => {
       res.status(200).json(go);
     })
@@ -21,13 +21,10 @@ router.get('/', ( req,res ) => {
 
 //=========================== Delete ========================================
 router.delete('/:id', validateProjectId, ( req,res ) => {
+
   project.remove(req.params.id)
     .then(count => {
-      if (count > 0) {
-        res.status(200).json({ message: "succesfully deleted"});
-      } else {
-        res.status(404).json({ message: "This item count not be found"});
-      }
+        res.status(200).json({ message: "successfully deleted"});
     })
     .catch(err => {
       console.log(err);
@@ -36,10 +33,34 @@ router.delete('/:id', validateProjectId, ( req,res ) => {
 })
 
 //============================= Put ==========================================
+router.put('/:id', validateProjectId, ( req,res ) => {
+  const info = req.body;
+  const {id} = req.params;
+
+  project.update(id, info)
+    .then(edit => {
+        res.status(200).json(edit);
+    })
+    .catch(err =>{
+      res.status(500).json({ errorMessage: "The project information could not be modified", err});
+    });
+});
+
+//=========================== Post ===============================
+router.post('/', validateProject, ( req, res ) => {
+  const info = req.body;
+
+  project.insert(info)
+    .then(go => {
+        res.status(200).json(go); 
+    })
+    .catch(err => {
+      res.status(500).json({errorMessage: "There was an error while saving the user to the projects database", err})
+    });
+});
 
 
-//=========================== Post ============================================
-
+// =========================== MiddleWare ===========================
 function validateProjectId(req, res, next) {
   const {id} = req.params;
   project.get(id)
@@ -55,5 +76,21 @@ function validateProjectId(req, res, next) {
       console.log(err);
       res.status(500).json({message: 'exception', err})
     });
-}
+};
+
+function validateProject(req, res, next) {
+  const body = req.body;
+  const { name } = body;
+  const { description } = body;
+  if (!body) {
+      res.status(400).json({ message: 'Please provide a project.' });
+  }
+  if (!name) {
+      res.status(400).json({ message: 'Please provide a name for your project.' });
+  }
+  if (!description) {
+      res.status(400).json({ message: 'Please provide a description of your project.' });
+  }
+  next();
+};
 module.exports = router;
